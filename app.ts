9e1e3a5e947ad8d6f5b847fcb11ee7fdc02e30de
Yash Diwaker <69832240+NoobCoderY@users.noreply.github.com>
2025-09-27 10:47:53 +0530

    Update CORS origin for frontend URL

--- app.ts
+++ app.ts
@@ -19,7 +19,7 @@ const app = express();
 
  app.use(cors({
     credentials: true,
-    origin:["http://localhost:3000","https://full-stack-task-board-frontend.vercel.app"],
+    origin:["http://localhost:3000","https://pabbl-fe.vercel.app/"],
   }))
   app.use(express.json());
   app.use(express.urlencoded({
